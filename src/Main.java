f56bcf48e86963d4b4bf26899a379eebfc0305ee
Mehdiobsi <Epulapp@epulapp117.univ-lyon1.fr>
2019-03-25 16:29:42 +0100

    Correctif

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Grille g = new Grille();
+		Console c = new Console(g);
+		g.addObserver(c);
+		(new Thread(g)).start();
+		
+	}
+
+}
